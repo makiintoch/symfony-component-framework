706e3d4a72adef365bf6753445180f994f0e07fe
makiintoch <makiintoch@gmail.com>
2018-10-03 14:34:30 +0300

    moved template files to src/pages

--- public/index.php
+++ public/index.php
@@ -11,14 +11,16 @@ $request = Request::createFromGlobals();
 $response = new Response();
 
 $map = [
-    '/hello' => __DIR__.'/../hello.php',
-    '/bye' => __DIR__.'/../bye.php',
+    '/hello' => __DIR__.'/../src/pages/hello.php',
+    '/bye' => __DIR__.'/../src/pages/bye.php',
 ];
 
 $path = $request->getPathInfo();
 
 if (isset($map[$path])) {
+    ob_start();
     require $map[$path];
+    $response->setContent(ob_get_clean());
 } else {
     $response->setStatusCode(404);
     $response->setContent('Not Found');
