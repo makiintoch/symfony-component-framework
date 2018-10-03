ce989d2794d6cdeabd166b0d99ce5232b3c4832f
makiintoch <makiintoch@gmail.com>
2018-10-03 17:30:41 +0300

    add render function

--- public/index.php
+++ public/index.php
@@ -11,6 +11,15 @@ use Symfony\Component\Routing\Matcher\UrlMatcher;
 use Symfony\Component\Routing\Exception\ResourceNotFoundException;
 
 
+function render($request)
+{
+    extract($request->attributes->all(), EXTR_SKIP);
+    ob_start();
+    require sprintf(__DIR__.'/../src/pages/%s.php', $_route);
+
+    return new Response(ob_get_clean());
+}
+
 $request = Request::createFromGlobals();
 $routes = require __DIR__.'/../src/routes.php';
 
@@ -19,10 +28,8 @@ $context->fromRequest($request);
 $matcher = new UrlMatcher($routes, $context);
 
 try {
-    extract($matcher->match($request->getPathInfo()), EXTR_SKIP);
-    ob_start();
-    require sprintf(__DIR__.'/../src/pages/%s.php', $_route);
-    $response = new Response(ob_get_clean());
+    $request->attributes->add($matcher->match($request->getPathInfo()));
+    $response = call_user_func($request->attributes->get('_controller'), $request);
 } catch (ResourceNotFoundException $e) {
     $response = new Response('Not Found', 404);
 } catch (Exception $e) {
--- src/routes.php
+++ src/routes.php
@@ -2,10 +2,35 @@
 
 use Symfony\Component\Routing\RouteCollection;
 use Symfony\Component\Routing\Route;
+use Symfony\Component\HttpFoundation\Response;
 
+
+function isLeapYear($year = null)
+{
+    if (null === $year) {
+        $year = date('Y');
+    }
+
+    return 0 === $year % 400 || (0 === $year % 4 && 0 !== $year % 100);
+}
 
 $routes = new RouteCollection();
-$routes->add('hello', new Route('/hello/{name}', ['name' => 'World']));
-$routes->add('bye', new Route('/bye'));
+$routes->add('hello', new Route('/hello/{name}', [
+    'name' => 'World',
+    '_controller' => 'render'
+]));
+$routes->add('bye', new Route('/bye', [
+    '_controller' => 'render'
+]));
+$routes->add('leap_year', new Route('/is-leap-year/{year}', [
+    'year' => null,
+    '_controller' => function ($request) {
+        if (isLeapYear($request->attributes->get('year'))) {
+            return new Response('Yep, this is a leap year.');
+        }
+
+        return new Response('Nope, this is not a leap year.');
+    }
+]));
 
 return $routes;
