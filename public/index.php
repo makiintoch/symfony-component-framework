29ebe0473459a6133a3ee1f970193a32c6b4c287
makiintoch <makiintoch@gmail.com>
2018-10-03 19:35:35 +0300

    add Kernel

--- public/index.php
+++ public/index.php
@@ -8,31 +8,21 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing;
 use Symfony\Component\Routing\RequestContext;
 use Symfony\Component\Routing\Matcher\UrlMatcher;
-use Symfony\Component\Routing\Exception\ResourceNotFoundException;
-use Symfony\Component\HttpKernel;
+use Symfony\Component\HttpKernel\Controller\ControllerResolver;
+use Symfony\Component\HttpKernel\Controller\ArgumentResolver;
+use App\Kernel;
 
 
 $request = Request::createFromGlobals();
-$routes = require __DIR__.'/../src/routes.php';
+$routes = require __DIR__.'/../config/routes.php';
 
 $context = new RequestContext();
-$context->fromRequest($request);
 $matcher = new UrlMatcher($routes, $context);
 
-$controllerResolver = new HttpKernel\Controller\ControllerResolver();
-$argumentResolver = new HttpKernel\Controller\ArgumentResolver();
+$controllerResolver = new ControllerResolver();
+$argumentResolver = new ArgumentResolver();
 
-try {
-    $request->attributes->add($matcher->match($request->getPathInfo()));
-
-    $controller = $controllerResolver->getController($request);
-    $arguments = $argumentResolver->getArguments($request, $controller);
-
-    $response = call_user_func_array($controller, $arguments);
-} catch (ResourceNotFoundException $e) {
-    $response = new Response('Page Not Found', 404);
-} catch (Exception $e) {
-    $response = new Response('Caught exception: '.$e->getMessage(), 500);
-}
+$kernel = new Kernel($matcher, $controllerResolver, $argumentResolver);
+$response = $kernel->handle($request);
 
 $response->send();
