3d56585d313d437fa5080e100680882a0396bae7
makiintoch <makiintoch@gmail.com>
2018-10-03 13:41:06 +0300

    add HttpFoundation component

--- index.php
+++ index.php
@@ -1,10 +1,16 @@
 <?php
 
-// check value is set
-$name = $_GET['name'] ?? 'World';
+require_once __DIR__.'/vendor/autoload.php';
 
-// add header
-//header('Content-Type: text/html; charset=utf-8');
 
-// XSS protection
-printf('Hello %s', htmlspecialchars($name, ENT_QUOTES, 'UTF-8'));
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+
+$request = Request::createFromGlobals();
+
+$name = $request->get('name', 'World');
+
+$response = new Response(sprintf('Hello %s', htmlspecialchars($name, ENT_QUOTES, 'UTF-8')));
+
+$response->send();
